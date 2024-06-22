6ffc00c453b2f25b27a9d592181c91a461fae254
samuelkwota <samuelkwota.sk@gmail.com>
2024-06-21 16:27:30 -0800

    uptate 2

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -36,10 +36,30 @@ VALUES
   ('Toyota', 'Camry', 2020, 'A reliable sedan', '/images/camry.jpg', '/images/camry_thumb.jpg', 2);
 
 -- Queries from Task One
+
+--Insert a new record into the account table
+INSERT INTO account (first_name, last_name, email, password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+--Modify the Tony Stark
+UPDATE account
+SET account_type = 'Admin'
+WHERE email = 'tony@starkent.com';
+
+-- Delete the Tony Stark record
+DELETE FROM account
+WHERE email = 'tony@starkent.com';
+
 -- Modify GM Hummer description
 UPDATE inventory
 SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
 WHERE make = 'GM' AND model = 'Hummer';
+
+-- Select make and model fields from inventory
+SELECT inventory.make, inventory.model, classification.classification_name
+FROM inventory
+INNER JOIN classification ON inventory.classification_id = classification.classification_id
+WHERE classification.classification_name = 'Sport';
 
 -- Update inv_image and inv_thumbnail paths
 UPDATE inventory
